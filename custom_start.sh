fc8fab08b49ceb93a00a72cd2faa9aad3320d261
Jean84j <127595608+Jean84j@users.noreply.github.com>
2025-05-31 15:03:08 +0300

    Update custom_start.sh

--- custom_start.sh
+++ custom_start.sh
@@ -7,34 +7,12 @@ echo "==> [2/5] Скачиваем модели (если ещё не скача
 mkdir -p ./models/checkpoints
 mkdir -p ./models/vae
 
-# if [ ! -f "./models/checkpoints/ponyDiffusionV6XL.safetensors" ]; then
-#   wget -O ./models/checkpoints/ponyDiffusionV6XL.safetensors \
-#     https://huggingface.co/mashb1t/fav_models/resolve/main/fav/ponyDiffusionV6XL.safetensors
-# fi
-
-# if [ ! -f "./models/vae/ponyDiffusionV6XL_vae.safetensors" ]; then
-#   wget -O ./models/vae/ponyDiffusionV6XL_vae.safetensors \
-#     https://huggingface.co/mashb1t/fav_models/resolve/main/fav/ponyDiffusionV6XL_vae.safetensors
-# fi
-
-# if [ ! -f "./models/checkpoints/waiNSFWIllustrious_v120.safetensors" ]; then
-#   wget -O ./models/checkpoints/waiNSFWIllustrious_v120.safetensors \
-#     # https://civitai.com/api/download/models/1490781?type=Model&format=SafeTensor&size=pruned&fp=fp16
-#     https://drive.google.com/file/d/1V53zegdkUV5jt2KOYHw0nF3EK4aCW0_O/view?usp=drive_link
-# fi
-
-  # https://civitai.com/api/download/models/799879?type=Model&format=SafeTensor&size=pruned&fp=bf16
-
-  # https://civitai.com/api/download/models/1692717?type=Model&format=SafeTensor&size=pruned&fp=fp16
-
-
 pip install -q gdown
 
-  # gdown --id 1V53zegdkUV5jt2KOYHw0nF3EK4aCW0_O -O ./models/checkpoints/waiNSFWIllustrious_v120.safetensors
+  # gdown --id 1V53zegdkUV5jt2KOYHw0nF3EK4aCW0_O -O ./models/checkpoints/cyberrealisticPony_semiRealV30.safetensors
 
-  gdown --id 1V53zegdkUV5jt2KOYHw0nF3EK4aCW0_O -O ./models/checkpoints/cyberrealisticPony_semiRealV30.safetensors
+  gdown --id 1Xp4XSVWDO6PCcAwWQ1LZNc4gdAE9hWPe -O ./models/checkpoints/cyberillustrious_v40.safetensors
 
-  
 
 echo "==> [3/5] Запускаем Fooocus..."
 python entry_with_update.py --share --always-high-vram
