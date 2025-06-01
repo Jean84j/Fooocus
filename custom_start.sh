339fcf98768a2abc643c4cace181bdbafb10a4a7
Jean84j <127595608+Jean84j@users.noreply.github.com>
2025-06-01 17:54:37 +0300

    Update custom_start.sh

--- custom_start.sh
+++ custom_start.sh
@@ -9,9 +9,9 @@ mkdir -p ./models/vae
 
 pip install -q gdown
 
-  # gdown --id 1V53zegdkUV5jt2KOYHw0nF3EK4aCW0_O -O ./models/checkpoints/cyberrealisticPony_semiRealV30.safetensors
+  gdown --id 1V53zegdkUV5jt2KOYHw0nF3EK4aCW0_O -O ./models/checkpoints/cyberrealisticPony_semiRealV30.safetensors
 
-  gdown --id 1Xp4XSVWDO6PCcAwWQ1LZNc4gdAE9hWPe -O ./models/checkpoints/cyberillustrious_v40.safetensors
+  # gdown --id 1Xp4XSVWDO6PCcAwWQ1LZNc4gdAE9hWPe -O ./models/checkpoints/cyberillustrious_v40.safetensors
 
 
 echo "==> [3/5] Запускаем Fooocus..."
