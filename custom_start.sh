46714148a8d6a61a670e994db70194a6bc21581b
Jean84j <127595608+Jean84j@users.noreply.github.com>
2025-06-02 12:39:39 +0300

    Update custom_start.sh

--- custom_start.sh
+++ custom_start.sh
@@ -5,10 +5,10 @@ mkdir -p ./models/vae
 
 pip install -q gdown
 
-  gdown --id 1V53zegdkUV5jt2KOYHw0nF3EK4aCW0_O -O ./models/checkpoints/cyberrealisticPony_semiRealV30.safetensors
-
   # gdown --id 1Xp4XSVWDO6PCcAwWQ1LZNc4gdAE9hWPe -O ./models/checkpoints/cyberillustrious_v40.safetensors
 
+   gdown --id 15rQGgOoPwUS416NnXtZtKVhAUz7RCXIb -O ./models/checkpoints/novaCartoonXL_v10.safetensors
 
+ 
 echo "==> [3/5] Запускаем Fooocus..."
 python entry_with_update.py --share --always-high-vram
