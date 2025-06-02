37f82f325a59e84fb6018a71f29fe4c5087859b6
Jean84j <127595608+Jean84j@users.noreply.github.com>
2025-06-02 19:03:44 +0300

    Update custom_start.sh

--- custom_start.sh
+++ custom_start.sh
@@ -5,9 +5,8 @@ mkdir -p ./models/vae
 
 pip install -q gdown
 
-  # gdown --id 1Xp4XSVWDO6PCcAwWQ1LZNc4gdAE9hWPe -O ./models/checkpoints/cyberillustrious_v40.safetensors
 
-   gdown --id 15rQGgOoPwUS416NnXtZtKVhAUz7RCXIb -O ./models/checkpoints/novaCartoonXL_v10.safetensors
+   gdown --id 1GT71_JQzBfOuvvpP8lMq91fAm-uJvYR9 -O ./models/checkpoints/illustriousXL20_v20.safetensors
 
  
 echo "==> [3/5] Запускаем Fooocus..."
