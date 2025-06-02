6cda8f65fac4386a9502d757bbfa6b86e6ededf2
Jean84j <127595608+Jean84j@users.noreply.github.com>
2025-06-02 19:26:00 +0300

    Update custom_start.sh

--- custom_start.sh
+++ custom_start.sh
@@ -6,8 +6,8 @@ mkdir -p ./models/vae
 pip install -q gdown
 
 
-   gdown --id 1GT71_JQzBfOuvvpP8lMq91fAm-uJvYR9 -O ./models/checkpoints/illustriousXL20_v20.safetensors
+   gdown --id 1WCI4-hxB2Y4boSkNljuds7Bsq99Vil-D -O ./models/checkpoints/ilustmix_v6.safetensors
 
- 
+
 echo "==> [3/5] Запускаем Fooocus..."
 python entry_with_update.py --share --always-high-vram
