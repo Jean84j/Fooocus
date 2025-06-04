d688ae2854ceeb6ee3a42051c9f29747c135dee1
Jean84j <127595608+Jean84j@users.noreply.github.com>
2025-06-04 13:41:23 +0300

    Update custom_start.sh

--- custom_start.sh
+++ custom_start.sh
@@ -6,13 +6,13 @@ mkdir -p ./models/vae
 pip install -q gdown
 
 
-   # gdown --id 1WCI4-hxB2Y4boSkNljuds7Bsq99Vil-D -O ./models/checkpoints/ilustmix_v6.safetensors
+   gdown --id 1WCI4-hxB2Y4boSkNljuds7Bsq99Vil-D -O ./models/checkpoints/ilustmix_v6.safetensors
 
    # gdown --id 11L0siJlm0FanLGymsXOgquGqr9xpiuJa -O ./models/checkpoints/nostraillustriousmix_mainMixVAELUNA.safetensors
 
     # gdown --id 1KZAXe3CYsgNWjXT7BtRAsWI1KxCq7sWU -O ./models/checkpoints/nostrarealisticmix_v10SDXLVAE.safetensors
 
-    gdown --id 1TXeQf0xvm_gTVjccgHoWWa3tCTlktiGl -O ./models/checkpoints/coolnameil_v4VAE.safetensors
+    # gdown --id 1TXeQf0xvm_gTVjccgHoWWa3tCTlktiGl -O ./models/checkpoints/coolnameil_v4VAE.safetensors
 
 
 echo "==> [3/5] Запускаем Fooocus..."
