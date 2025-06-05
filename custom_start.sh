13fcce3c2b7f6c8a401e79ed6802155340644c59
Jean84j <127595608+Jean84j@users.noreply.github.com>
2025-06-05 10:55:27 +0300

    Update custom_start.sh

--- custom_start.sh
+++ custom_start.sh
@@ -8,11 +8,11 @@ pip install -q gdown
 
  # gdown --id 1WCI4-hxB2Y4boSkNljuds7Bsq99Vil-D -O ./models/checkpoints/ilustmix_v6.safetensors
 
- gdown --id 1_HoBW4U4-Y9rDlp9dkEIEC2jhvI73V8a -O ./models/checkpoints/waiNSFWIllustrious_v140.safetensors
+ # gdown --id 1_HoBW4U4-Y9rDlp9dkEIEC2jhvI73V8a -O ./models/checkpoints/waiNSFWIllustrious_v140.safetensors
 
  # gdown --id 11L0siJlm0FanLGymsXOgquGqr9xpiuJa -O ./models/checkpoints/nostraillustriousmix_mainMixVAELUNA.safetensors
 
- # gdown --id 1KZAXe3CYsgNWjXT7BtRAsWI1KxCq7sWU -O ./models/checkpoints/nostrarealisticmix_v10SDXLVAE.safetensors
+ gdown --id 1KZAXe3CYsgNWjXT7BtRAsWI1KxCq7sWU -O ./models/checkpoints/nostrarealisticmix_v10SDXLVAE.safetensors
 
  # gdown --id 1TXeQf0xvm_gTVjccgHoWWa3tCTlktiGl -O ./models/checkpoints/coolnameil_v4VAE.safetensors
 
