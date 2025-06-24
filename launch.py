078efd442adbfcb8ad0335f583ac3ccb373b18ff
Jean84j <127595608+Jean84j@users.noreply.github.com>
2025-06-24 09:59:59 +0300

    Update launch.py

--- launch.py
+++ launch.py
@@ -174,7 +174,9 @@ custom_models = [
 
     # ("1rP8EYrPdxFhTKP7njcSb7-N-wu_SLihI", "./models/checkpoints/IL_novaCartoonXL_v20.safetensors"),
 
-     ("1CsHcxXTQOB_25UARq8C4B8Vg2AGP6ORF", "./models/checkpoints/IL_JedpointReal.safetensors"),
+     # ("1CsHcxXTQOB_25UARq8C4B8Vg2AGP6ORF", "./models/checkpoints/IL_JedpointReal.safetensors"),
+
+     ("1mTmr_7yeekZjNbbsRNbdoUpDJlXFalng", "./models/checkpoints/SD_realcartoon3d_v18.safetensors"),
 
 
  # gdown --id 1KZAXe3CYsgNWjXT7BtRAsWI1KxCq7sWU -O ./models/checkpoints/nostrarealisticmix_v10SDXLVAE.safetensors
