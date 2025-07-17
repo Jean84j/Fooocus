94e7f76dd388fe5b815fc1cabadd2e266f380281
Jean84j <127595608+Jean84j@users.noreply.github.com>
2025-07-17 08:59:51 +0300

    Update launch.py

--- launch.py
+++ launch.py
@@ -171,7 +171,7 @@ custom_models = [
 
     # ("1bt8BsjP2N9E1lXxawzVv-WIfJARDtzDo", "./models/checkpoints/PN_animergePonyXL_v60.safetensors"),
 
-    ("1dMSbJoFG4MOVcqRRXpbK0HN8PdYyFW3-", "./models/checkpoints/IL_ilustmix_v70Cinematic.safetensors"),
+    # ("1dMSbJoFG4MOVcqRRXpbK0HN8PdYyFW3-", "./models/checkpoints/IL_ilustmix_v70Cinematic.safetensors"),
 
     # ("11L0siJlm0FanLGymsXOgquGqr9xpiuJa", "./models/checkpoints/IL_nostraillustriousmix_mainMixVAELUNA.safetensors"),
 
@@ -179,7 +179,7 @@ custom_models = [
 
      # ("1CsHcxXTQOB_25UARq8C4B8Vg2AGP6ORF", "./models/checkpoints/IL_JedpointReal.safetensors"),
 
-     # ("1S8jTIvz8q5QYWz_ieo58_C2iTwfOuHyb", "./models/checkpoints/IL_waiNSFWIllustrious_v140.safetensors"),
+     ("1S8jTIvz8q5QYWz_ieo58_C2iTwfOuHyb", "./models/checkpoints/IL_waiNSFWIllustrious_v140.safetensors"),
 
 
  # gdown --id 1KZAXe3CYsgNWjXT7BtRAsWI1KxCq7sWU -O ./models/checkpoints/nostrarealisticmix_v10SDXLVAE.safetensors
