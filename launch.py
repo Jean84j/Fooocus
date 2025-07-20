08e08070d41b5c3684cc9afdc1298e6a301013de
Jean84j <127595608+Jean84j@users.noreply.github.com>
2025-07-20 08:30:33 +0300

    Update launch.py

--- launch.py
+++ launch.py
@@ -169,9 +169,9 @@ custom_models = [
         # Model
     # ("1WCI4-hxB2Y4boSkNljuds7Bsq99Vil-D", "./models/checkpoints/IL_ilustmix_v6.safetensors")
 
-    # ("1bt8BsjP2N9E1lXxawzVv-WIfJARDtzDo", "./models/checkpoints/PN_animergePonyXL_v60.safetensors"),
+    ("1bt8BsjP2N9E1lXxawzVv-WIfJARDtzDo", "./models/checkpoints/PN_animergePonyXL_v60.safetensors"),
 
-    ("1dMSbJoFG4MOVcqRRXpbK0HN8PdYyFW3-", "./models/checkpoints/IL_ilustmix_v70Cinematic.safetensors"),
+    # ("1dMSbJoFG4MOVcqRRXpbK0HN8PdYyFW3-", "./models/checkpoints/IL_ilustmix_v70Cinematic.safetensors"),
 
     # ("11L0siJlm0FanLGymsXOgquGqr9xpiuJa", "./models/checkpoints/IL_nostraillustriousmix_mainMixVAELUNA.safetensors"),
 
