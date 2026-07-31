b99051a2d797dca2239c3f6a8e4cfe89c67fbf84
Jean84j <127595608+Jean84j@users.noreply.github.com>
2026-07-31 13:44:22 +0300

    Update launch.py

--- launch.py
+++ launch.py
@@ -179,7 +179,7 @@ custom_models = [
 
         # Model
    
-    ("1qY5im7L2gtGHb4thr7iMVwUbwUooj4E4", "./models/checkpoints/SDXL_lustifySDXLNSFW_ggwpV7.safetensors"),
+    # ("1qY5im7L2gtGHb4thr7iMVwUbwUooj4E4", "./models/checkpoints/SDXL_lustifySDXLNSFW_ggwpV7.safetensors"),
 
     # ("1gh5x1P6FQtQDY_kIjYEKoiLa8MzPoeFi", "./models/checkpoints/IL_ilustmix_v111.safetensors"),
 
